aa2278d2c1b521636b0b990ff03b96592fd526f2
urva9 <159683192+urva9@users.noreply.github.com>
2024-06-17 12:30:18 +0530

    Update options.py

--- src/utils/options.py
+++ src/utils/options.py
@@ -1,7 +1,3 @@
-# ------------------------------------------------------------------------
-# Copyright (c) 2023 CandleLabAI. All Rights Reserved.
-# ------------------------------------------------------------------------
-
 import yaml
 from collections import OrderedDict
 
